700f2d7165fb7aae9b74429c645fd9b10ca1a814
Lance <pianoman13c@gmail.com>
2015-03-21 20:07:56 -0400

    Nav Bar first revision complete

---
+++ Version3/main.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Lance on 3/21/2015.
+ */
+
+floatNav = function() {
+    console.log("test");
+    //var k = window.onscroll();
+    $("#navBar").mouseenter(function(){
+       $("#navBar").css("position", "fixed");
+       //$("a").css("color", "red");
+       console.log("test 2");
+    });
+};
+
+/*
+$(function(){
+    //$("#navBar").css("font-size",
+      //  function () {
+        //    return window.outerWidth / 50;
+        //}
+    //);
+    $(window).bind('resize',function(){
+       var preferredHeight = 600;
+       var fontsize = 18;
+
+       var displayHeight = $(window).height();
+       var percentage = displayHeight / preferredHeight;
+       var newFontSize = Math.floor(fontsize * percentage);
+       $("#navBar").css("font-size", newFontSize);
+    });
+    console.log("change");
+});
+*/
--- Version3/navFloat.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Lance on 3/21/2015.
- */
-
-floatNav = function() {
-    //var k = window.onscroll();
-
-};
