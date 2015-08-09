edd875b886ee849d98473266603939e3dc558ce2
lance13c <pianoman13c@gmail.com>
2015-08-09 15:13:02 -0400

    New Website Version using uikit and angularJS

---
+++ AngularV2/angular/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Lance on 8/9/2015.
+ */
---
+++ AngularV2/gulpfile.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Lance on 8/9/2015.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
+
+var paths = {
+    sass: ['./sass/*.scss'],
+    images: ['./img/**/*.*'],
+    index: ['./index.html'],
+    dep: ['./dependencies/**'],
+    angular: ['./angular/**']
+};
+
+gulp.task('clean', function(cb) {
+    del(['target'], cb);
+});
+
+gulp.task('angular', function() {
+    gulp.src(paths.angular)
+        .pipe(gulp.dest('./target/angular'));
+});
+
+gulp.task('dep', function() {
+    gulp.src(paths.dep)
+        .pipe(gulp.dest('./target/dependencies'));
+});
+
+// Compile SCSS to CSS
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths : [paths.sass]
+        }))
+        .pipe(gulp.dest('./target/css'));
+});
+
+gulp.task('index', function() {
+    gulp.src(paths.index)
+        .pipe(gulp.dest('./target'));
+});
+
+// Copy all static img
+gulp.task('images', function() {
+    return gulp.src(paths.images)
+        .pipe(imagemin({optimizationLevel: 2}))
+        .pipe(gulp.dest('target/img'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.index, ['index']);
+    gulp.watch((paths.dep, ['dep']));
+    gulp.watch((paths.angular, ['angular']));
+});
+
+gulp.task('default', ['watch', 'sass', 'images', 'index', 'dep', 'angular']);
