28835322095755f792a768b9aef7ceb9010e6aa0
lance13c <pianoman13c@gmail.com>
2015-08-09 15:13:54 -0400

    New Website Version using uikit and angularJS

--- AngularJSVersion/Dependency/angular.js
+++ AngularJSVersion/Dependency/angular.js
@@ -14603,7 +14603,7 @@
 
     /**
      * @description
-     * Private service to sanitize uris for links and images. Used by $compile and $sanitize.
+     * Private service to sanitize uris for links and img. Used by $compile and $sanitize.
      */
     function $$SanitizeUriProvider() {
         var aHrefSanitizationWhitelist = /^\s*(https?|ftp|mailto|tel|file):/,
