ebdd8cd8b56007d6eed7a04323b962cf1be98cd2
lance13c <pianoman13c@gmail.com>
2015-08-10 00:33:36 -0400

    AngularJs is working

---
+++ AngularV2/target/angular/app.js
@@ -0,0 +1,12 @@
+var mod = angular.module("mod", []);
+mod.directive('csApp', function(){
+    return {
+        scope: {},
+        restrict: 'E',
+        templateUrl: './angular/cs-app.html'
+    };
+});
+
+var el = document.createElement('cs-app');
+angular.bootstrap(el, [mod.name]);
+$('.cs-app-class').append(el);
