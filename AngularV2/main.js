3589d0852ec73ff3b05063140e17505e2099a2a9
lance13c <pianoman13c@gmail.com>
2015-10-21 00:57:56 -0400

    Working on setting up static hosting of files with nodejs

--- AngularV2/main.js
+++ AngularV2/main.js
@@ -1,12 +1,36 @@
 /**
  * Created by Lance on 10/19/2015.
  */
-var http = require('http');
+http = require('http');
+path = require("path");
+url = require("url");
+fs = require("fs");
 
 const PORT = 80;
 
 function handleRequest(request, response){
-    response.end('Success, Hit Path: ' + request.url);
+    var my_path = url.parse(request.url).pathname;
+    var full_path = path.join(process.cwd(), my_path);
+
+    path.access(full_path, fs.F_OK, function (err){
+        if (err){
+            response.writeHeader(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+        }else{
+            fs.readFile(full_path, "binary", function(err, file){
+                if (err){
+                    response.writeHeader(500, {"Content-Type": "text/plain"});
+                    response.write("err" + "\n");
+                    response.end();
+                }else{
+                    response.writeHeader(200);
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
 }
 
 // Creating a server
