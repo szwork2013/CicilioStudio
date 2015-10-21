df8de2b315e3bac1da0552a4b0801253bd8eb32b
lance13c <pianoman13c@gmail.com>
2015-10-21 02:18:23 -0400

    Basic Express Hello World server working

--- AngularV2/main.js
+++ AngularV2/main.js
@@ -1,42 +1,17 @@
 /**
  * Created by Lance on 10/19/2015.
  */
-http = require('http');
-path = require("path");
-url = require("url");
-fs = require("fs");
+var express = require('express');
+var app = express();
 
 const PORT = 80;
 
-function handleRequest(request, response){
-    var my_path = url.parse(request.url).pathname;
-    var full_path = path.join(process.cwd(), my_path);
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
 
-    path.access(full_path, fs.F_OK, function (err){
-        if (err){
-            response.writeHeader(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-        }else{
-            fs.readFile(full_path, "binary", function(err, file){
-                if (err){
-                    response.writeHeader(500, {"Content-Type": "text/plain"});
-                    response.write("err" + "\n");
-                    response.end();
-                }else{
-                    response.writeHeader(200);
-                    response.write(file, "binary");
-                    response.end();
-                }
-            });
-        }
-    });
-}
+var server = app.listen(PORT, function () {
+    //var host = server.address().address;
 
-// Creating a server
-var server = http.createServer(handleRequest);
-
-// Starting the Server
-server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log('App Listening on port %s', PORT);
 });
