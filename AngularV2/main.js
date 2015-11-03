edaac960656b2439cde579c29b2d3c418426f2ed
lance13c <pianoman13c@gmail.com>
2015-11-03 09:24:10 -0500

    Update with express

--- AngularV2/main.js
+++ AngularV2/main.js
@@ -2,16 +2,31 @@
  * Created by Lance on 10/19/2015.
  */
 var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.csr', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
 var app = express();
 
-const PORT = 80;
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+var cs_ports = {http: 80, https: 443 };
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
 
-var server = app.listen(PORT, function () {
+httpServer.listen(cs_ports.http, function () {
     //var host = server.address().address;
-
-    console.log('App Listening on port %s', PORT);
+    console.log('App Listening on port %s', cs_ports.http);
 });
+
+httpsServer.listen(cs_ports.https, function () {
+    console.log('App Listening on port %s', cs_ports.https);
+});
+
