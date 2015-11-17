7da3f4b173a0495a19b81c724ea3a9a446c29ba6
lance13c <pianoman13c@gmail.com>
2015-11-17 00:57:02 -0500

    Static Files Now Serving Properly

--- AngularV2/main.js
+++ AngularV2/main.js
@@ -7,27 +7,34 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
-var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.csr', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+//var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+//var certificate = fs.readFileSync('sslcert/server.csr', 'utf8');
+//var credentials = {key: privateKey, cert: certificate};
 
 var app = express();
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+//var httpsServer = https.createServer(credentials, app);
+
+//Redefine where views are located
+//app.set('views',`${__dirname}/webapp/*`);
+
+//app.set('view engine', 'html');
+//Hosts the static files in public under /assets
+app.use(express.static(`${__dirname}/target`));
 
 var cs_ports = {http: 80, https: 443 };
 
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
+//app.get('/', function(req, res){
+//    res.send('yo');
+//});
 
 httpServer.listen(cs_ports.http, function () {
     //var host = server.address().address;
     console.log('App Listening on port %s', cs_ports.http);
 });
 
-httpsServer.listen(cs_ports.https, function () {
-    console.log('App Listening on port %s', cs_ports.https);
-});
+//httpsServer.listen(cs_ports.https, function () {
+    //console.log('App Listening on port %s', cs_ports.https);
+//});
 
