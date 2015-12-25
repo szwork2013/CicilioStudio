b298fe7082619665b96eada145f86ff3b883aa0a
lance13c <pianoman13c@gmail.com>
2015-12-25 04:46:17 -0500

    Added temp chrismus card for parents

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ var del = require('del');
 
 var paths = {
     sass: ['./webapp/assets/sass/*.scss'],
-    images: ['./webapp/assets/img/**/*.*'],
-    index: ['./webapp/index.html'],
+    images: ['./webapp/assets/img/**'],
+    views: ['./webapp/views/*.html'],
+    main: ['./webapp/*.*'],
     dep: ['./webapp/dependencies/**'],
     angular: ['./webapp/angular/*.*']
 };
@@ -23,6 +24,11 @@ gulp.task('clean', function(cb) {
 gulp.task('angular', function() {
     gulp.src(paths.angular)
         .pipe(gulp.dest('./target/angular'));
+});
+
+gulp.task('views', function() {
+    gulp.src(paths.views)
+        .pipe(gulp.dest('./target/views'));
 });
 
 gulp.task('dep', function() {
@@ -37,11 +43,11 @@ gulp.task('sass', function() {
             outputStyle: 'compressed',
             includePaths : [paths.sass]
         }))
-        .pipe(gulp.dest('./target/css'));
+        .pipe(gulp.dest('./target/assets/css'));
 });
 
-gulp.task('index', function() {
-    gulp.src(paths.index)
+gulp.task('main', function() {
+    gulp.src(paths.main)
         .pipe(gulp.dest('./target'));
 });
 
@@ -49,15 +55,16 @@ gulp.task('index', function() {
 gulp.task('images', function() {
     return gulp.src(paths.images)
         .pipe(imagemin({optimizationLevel: 2}))
-        .pipe(gulp.dest('target/img'));
+        .pipe(gulp.dest('target/assets/img'));
 });
 
 gulp.task('watch', function () {
     gulp.watch(paths.sass, ['sass']);
     gulp.watch(paths.images, ['images']);
-    gulp.watch(paths.index, ['index']);
+    gulp.watch(paths.main, ['main']);
     gulp.watch(paths.dep, ['dep']);
     gulp.watch(paths.angular, ['angular']);
+    gulp.watch(paths.views, ['views']);
 });
 
-gulp.task('default', ['watch', 'sass', 'images', 'index', 'angular']);
+gulp.task('default', ['watch', 'views', 'sass', 'images', 'main', 'angular']);
--- webapp/angular/app.js
+++ webapp/angular/app.js
@@ -6,7 +6,19 @@ mod.directive('csApp', function(){
         templateUrl: './angular/cs-app.html'
     };
 });
+
 //cs: cicilio studio
 var el = document.createElement('cs-app');
 angular.bootstrap(el, [mod.name]);
 $('.cs-app-class').append(el);
+
+mod.config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+        .state('home', {
+           url: "/",
+           templateUrl: "views/home.html"
+        });
+});
