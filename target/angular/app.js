e8a04f7714fd7bb944daf2ddd900c8759c6031b1
lance13c <pianoman13c@gmail.com>
2016-01-05 23:59:29 -0500

    added projects dropdown, sass animations, formating, sidenav colors

--- target/angular/app.js
+++ target/angular/app.js
@@ -44,12 +44,27 @@ mod.directive("csMain", function(){
                 '<a href="#!" class="cs-breadcrumb valign">Second</a>',
                 '<a href="#!" class="cs-breadcrumb valign">Third</a>'
             ];
+
+            //Sets up mobile side nav
             var menu = $('.cs-mobile-menu');
             menu.sideNav({
                 menuWidth: 200,
                 edge: 'right',
                 closeOnClick: true
             });
+
+            //Sets up projects dropdown button
+            var dropdownProject = $('.dropdown-button');
+            dropdownProject.dropdown({
+                    inDuration: 300,
+                    outDuration: 225,
+                    constrain_width: false, // Does not change width of dropdown to that of the activator
+                    hover: false, // Activate on hover
+                    gutter: 0, // Spacing from edge
+                    belowOrigin: true, // Displays dropdown below the button
+                    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+                }
+            );
         },
         templateUrl: './views/main.html'
     };
